8b0777037ab79d410dc89e5ce597e351e488c3ca
BlackCatQ8 <kuwaitmoon2323@gmail.com>
2021-06-20 09:55:35 +0300

    BASE

--- Includes/MainSearch.php
+++ Includes/MainSearch.php
@@ -3,7 +3,7 @@
     <h4> البحث </h4>
 </div>
 <div id="Context">
-    <form id="search-main" method="GET" action="index.php">
+    <form id="search-main" method="GET" action="index.html">
         <input class="form-control inline" type="text" name="q" placeholder="البحث"  />
         <button class="btn btn-dark" type="submit" name="submit" value="search"> إبحث </button>
     </form>
